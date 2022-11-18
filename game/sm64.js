7dce89cf165e2c510d9a66673c5387665da14c87
heysnp <112211472+heys1234@users.noreply.github.com>
2022-11-18 06:40:41 +0000

    i will be changing the controls

--- game/sm64.js
+++ game/sm64.js
@@ -1665,7 +1665,7 @@ function isDataURI(filename) {
 
 
 
-var wasmBinaryFile = 'sm64.wasm';
+var wasmBinaryFile = '../game/sm64.wasm';
 if (!isDataURI(wasmBinaryFile)) {
   wasmBinaryFile = locateFile(wasmBinaryFile);
 }
